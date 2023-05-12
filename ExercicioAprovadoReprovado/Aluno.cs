f2a00fa4ff8c9d372139d1743187bb1fa4f5a94d
metal <metal@DESKTOP-ECM02DI>
2023-05-12 12:25:27 -0300

    Add project files.

---
+++ ExercicioAprovadoReprovado/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioAprovadoReprovado
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public double SomaNotas()
+        {
+            return (Nota1 + Nota2 + Nota3) / 100;
+        }
+        public double NotaResta()
+        {
+            return (60.00 - SomaNotas());
+        }
+    }
+}
---
+++ ExercicioAprovadoReprovado/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace ExercicioAprovadoReprovado
+{
+    class AprovadoReprovado
+    {
+        static void Main(string[] args)
+        {
+            
+            Aluno aluno = new Aluno();
+
+            Console.Write("Digite o nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine());
+            aluno.Nota2 = double.Parse(Console.ReadLine());
+            aluno.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("NOTA FINAL = " + aluno.SomaNotas().ToString("F2", CultureInfo.InvariantCulture));
+            if (aluno.SomaNotas() >= 60.00){
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTAM " + aluno.NotaResta().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }        
+
+    }
+}
